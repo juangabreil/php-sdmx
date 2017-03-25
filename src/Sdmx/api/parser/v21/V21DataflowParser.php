131b954229860c679d6bc18487c4ed3a55d6f386
Juan Gabriel Jimenez Campos <juangabreil@gmail.com>
2017-03-25 09:57:40 +0100

    Added namespace removal step in dataflow parser.

--- src/Sdmx/api/parser/v21/V21DataflowParser.php
+++ src/Sdmx/api/parser/v21/V21DataflowParser.php
@@ -6,6 +6,7 @@ namespace Sdmx\api\parser\v21;
 use Sdmx\api\entities\Dataflow;
 use Sdmx\api\entities\DsdIdentifier;
 use Sdmx\api\parser\DataflowParser;
+use Sdmx\api\parser\ParserUtils;
 use SimpleXMLElement;
 
 class V21DataflowParser implements DataflowParser
@@ -21,22 +22,23 @@ class V21DataflowParser implements DataflowParser
     public function parse($data)
     {
         $result = array();
-        $xml = new SimpleXMLElement($data);
+        $dataWoNs = ParserUtils::removeNamespaces($data);
+        $xml = new SimpleXMLElement($dataWoNs);
 
-        $flows = $xml->xpath('/mes:Structure/mes:Structures/str:Dataflows/str:Dataflow');
+        $flows = $xml->xpath('/Structure/Structures/Dataflows/Dataflow');
         foreach ($flows as $flow) {
             $dataflow = new Dataflow();
             $dataflow->setId((string) $flow[self::ID]);
             $dataflow->setAgency((string) $flow[self::AGENCY_ID]);
             $dataflow->setVersion((string)$flow[self::VERSION]);
 
-            $name = $flow->xpath('./com:Name[@xml:lang="en"]');
+            $name = $flow->xpath('./Name[@lang="en"]');
             if(count($name) > 0){
                 $dataflow->setName((string)$name[0]);
             }
 
 
-            $ref = $flow->xpath('./str:Structure/Ref');
+            $ref = $flow->xpath('./Structure/Ref');
             if(count($ref) > 0){
                 $ref = $ref[0];
                 $dsd = new DsdIdentifier(
